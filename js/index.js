e6e10de3b40904dfc8d8ae5e196769b49c983746
devb0x <moniezgeoffrey59@gmail.com>
2021-05-07 07:49:27 +0200

    fixing .btn-active class render

--- js/index.js
+++ js/index.js
@@ -1,34 +1,29 @@
 import { PhotographersList } from "./class/PhotographersList.js";
 
 /**
- * Filter photographers from #tags
+ * Remove .btn-active class on buttons
  */
-// for (let i = 0; i < btn.length; i++) {
-//   btn[i].addEventListener('click', e => {
-//     resetActive();
-//     e.target.classList.add('btn-active');
-//     photographersList.filter(e);
-//   })
-// }
-
-  let buttons = document.querySelectorAll(".filter-btn");
 function resetActive() {
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].classList.remove('btn-active');
   }
-
 }
 
-const body = document.body;
-
-body.addEventListener('click', (e) => {
-  console.log(e.target)
-  if (e.target && e.target.matches("button.filter-btn")) {
-    // console.log('aze')
-    resetActive();
-    photographersList.filter(e);
-  }
-});
+/**
+ * remove and add .btn-active class when we filter
+ * @type {NodeListOf<Element>}
+ */
+let buttons = document.querySelectorAll(".filter-btn");
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (e) => {
+      // console.log('event clic filter');
+        resetActive();
+      if (e.target.matches("button.filter-btn")) {
+        e.target.classList.add('btn-active');
+        photographersList.filter(e);
+      }
+  })
+}
 
 const photographersList = new PhotographersList();
 photographersList.getPhotographers().then(() => photographersList.render());
