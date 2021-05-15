b9fabe0555e361d0e363a1cdbc04f8a9c1cca3b8
devb0x <moniezgeoffrey59@gmail.com>
2021-05-15 11:04:30 +0200

    fix next and prev when we're on the first and last element add display title on lightbox

--- js/class/Lightbox.js
+++ js/class/Lightbox.js
@@ -1,6 +1,10 @@
 import { photographerPath } from "../page2.js";
 
 const lightBox_div = document.getElementById('lightbox')
+const lightBoxContainer_div = document.querySelector('.lightbox__container')
+const lightBoxClose_btn = document.querySelector('.lightbox-btn__close')
+const lightBoxPrev_btn = document.querySelector('.lightbox-btn__prev')
+const lightBoxNext_btn = document.querySelector('.lightbox-btn__next')
 
 export class Lightbox {
 
@@ -10,22 +14,39 @@ export class Lightbox {
     this.index = null
   }
 
+  /**
+   * Remove the <img> inside the container of the lightbox
+   */
+  reset() {
+    lightBoxContainer_div.innerHTML = ''
+  }
+
   next() {
+
     console.log('next image')
     this.index += 1
     console.log('index after next : ' + this.index)
 
-    const lightbox_container_div = document.createElement('div')
-    lightbox_container_div.classList.add('lightbox__container')
+    /**
+     * If we are on the last element, display the first one
+     */
+    if (this.index > this.mediaList.length -1) {
+      this.index = 0
+    }
+
+    this.reset()
 
     const containerImg = document.createElement('img')
     containerImg.classList.add('lightbox__container-img')
     containerImg.src= `./../Sample%20Photos/${photographerPath}/${this.mediaList[this.index].image}`
-    lightbox_container_div.append(containerImg)
-    lightBox_div.append(lightbox_container_div)
 
-    // containerImg.src= `./../Sample%20Photos/${photographerPath}/${this.mediaList[this.index].image}`
+    lightBoxContainer_div.append(containerImg)
 
+    const titleDiv = document.createElement('div')
+    titleDiv.classList.add('lightbox__container-title')
+    titleDiv.innerHTML = `${this.mediaList[this.index].title}`
+
+    lightBoxContainer_div.append(titleDiv)
   }
 
   prev() {
@@ -33,21 +54,32 @@ export class Lightbox {
     this.index -= 1
     console.log('index after prev : ' + this.index)
 
-    // lightBox_div.innerHTML = ''
+    /**
+     * If we are on first element, display the last one
+     */
+    if (this.index < 0) {
+      this.index = this.mediaList.length -1
+    }
 
-    const lightbox_container_div = document.createElement('div')
-    lightbox_container_div.classList.add('lightbox__container')
+    this.reset()
 
     const containerImg = document.createElement('img')
     containerImg.classList.add('lightbox__container-img')
     containerImg.src= `./../Sample%20Photos/${photographerPath}/${this.mediaList[this.index].image}`
-    lightbox_container_div.append(containerImg)
-    lightBox_div.append(lightbox_container_div)
+
+    lightBoxContainer_div.append(containerImg)
+
+    const titleDiv = document.createElement('div')
+    titleDiv.classList.add('lightbox__container-title')
+    titleDiv.innerHTML = `${this.mediaList[this.index].title}`
+
+    lightBoxContainer_div.append(titleDiv)
   }
+
 
   close() {
     lightBox_div.style.display = "none"
-    lightBox_div.innerHTML = ''
+    lightBoxContainer_div.innerHTML = ''
   }
 
   render(idMedia) {
@@ -62,100 +94,81 @@ export class Lightbox {
       if (this.activeId === this.mediaList[i].id) {
         this.index = this.mediaList.indexOf(this.mediaList[i])
 
-        /**
-         * Close button
-         * @type {HTMLButtonElement}
-         */
-        const close_btn = document.createElement('button')
-        close_btn.classList.add('lightbox__close')
-        close_btn.innerText = 'close'
-        lightBox_div.append(close_btn)
-        close_btn.addEventListener('click', (e) => {
-          this.close()
-        })
-
-        // /**
-        //  * Next button
-        //  * @type {HTMLButtonElement}
-        //  */
-        // const next_btn = document.createElement('button')
-        // next_btn.classList.add('lightbox__next')
-        // next_btn.innerText = 'next'
-        // lightBox_div.append(next_btn)
-        // next_btn.addEventListener('click', (e) => {
-        //   this.next()
-        // })
-        //
-        // /**
-        //  * Previous button
-        //  * @type {HTMLButtonElement}
-        //  */
-        // const prev_btn = document.createElement('button')
-        // prev_btn.classList.add('lightbox__prev')
-        // prev_btn.innerText = 'prev'
-        // lightBox_div.append(prev_btn)
-        // prev_btn.addEventListener('click', (e) => {
-        //   this.prev()
-        // })
-
-        /**
-         * Container for img
-         * @type {HTMLDivElement}
-         */
-        const lightBox__container = document.createElement('div')
-        lightBox__container.classList.add('lightbox__container')
-        lightBox_div.appendChild(lightBox__container)
-
-
-    // for (let i = 0; i < this.mediaList.length; i++) {
-    //
-    //   // this.index = this.mediaList.indexOf(this.mediaList[i])
-    //
-    //
-    //   if (this.activeId === this.mediaList[i].id) {
-    //   this.index = this.mediaList.indexOf(this.mediaList[i])
-
-        /**
-         * Next button
-         * @type {HTMLButtonElement}
-         */
-        const next_btn = document.createElement('button')
-        next_btn.classList.add('lightbox__next')
-        next_btn.innerText = 'next'
-        lightBox_div.append(next_btn)
-        next_btn.addEventListener('click', (e) => {
-          this.next()
-          lightBox__container.remove()
-        })
-
-        /**
-         * Previous button
-         * @type {HTMLButtonElement}
-         */
-        const prev_btn = document.createElement('button')
-        prev_btn.classList.add('lightbox__prev')
-        prev_btn.innerText = 'prev'
-        lightBox_div.append(prev_btn)
-        prev_btn.addEventListener('click', (e) => {
-          this.prev()
-          lightBox__container.remove()
-        })
-
         console.error(this.mediaList.indexOf(this.mediaList[i])) // ok
 
         /**
-         * <img>
+         * Create <img> inside the lightbox container
          * @type {HTMLImageElement}
          */
         const containerImg = document.createElement('img')
         containerImg.classList.add('lightbox__container-img')
         containerImg.src= `./../Sample%20Photos/${photographerPath}/${this.mediaList[i].image}`
-        lightBox__container.append(containerImg)
+        lightBoxContainer_div.append(containerImg)
 
+        /**
+         * Create <div> for the title image
+         * @type {HTMLDivElement}
+         */
+        const titleDiv = document.createElement('div')
+        titleDiv.classList.add('lightbox__container-title')
+        titleDiv.innerHTML = `${this.mediaList[i].title}`
+        lightBoxContainer_div.append(titleDiv)
       }
     }
 
+    /**
+     * Event Listener for close prev and next
+     */
+    lightBoxClose_btn.addEventListener('click', () => {
+      this.close()
+    })
+
+    lightBoxPrev_btn.addEventListener('click', () => {
+      this.prev()
+    })
+
+    lightBoxNext_btn.addEventListener('click', () => {
+      this.next()
+    })
   }
 
-
 }
+
+
+
+/**
+ * Close button
+ * @type {HTMLButtonElement}
+ */
+// const close_btn = document.createElement('button')
+// close_btn.classList.add('lightbox__close')
+// close_btn.innerText = 'close'
+// lightBox_div.append(close_btn)
+// close_btn.addEventListener('click', (e) => {
+//   this.close()
+// })
+
+
+/**
+ * Next button
+ * @type {HTMLButtonElement}
+ */
+// const next_btn = document.createElement('button')
+// next_btn.classList.add('lightbox__next')
+// next_btn.innerText = 'next'
+// lightBox_div.append(next_btn)
+// next_btn.addEventListener('click', (e) => {
+//   this.next()
+// })
+//
+/**
+ * Previous button
+ * @type {HTMLButtonElement}
+ */
+// const prev_btn = document.createElement('button')
+// prev_btn.classList.add('lightbox__prev')
+// prev_btn.innerText = 'prev'
+// lightBox_div.append(prev_btn)
+// prev_btn.addEventListener('click', (e) => {
+//   this.prev()
+// })
